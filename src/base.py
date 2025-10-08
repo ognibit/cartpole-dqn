d83db11390f2f2b65b21fd44ab5f1f3690e2b2dd
Omar Rampado <o.rampado@student.vu.nl>
2025-10-08 14:50:46 +0200

    Global structure of the DQN algorithm, without the QNet optimization
    Implemented features:
    - PyTorch  device identification
    - Epsilon-greedy behaviour policy
    - Experience Replay Buffer
    - Target Network weights update
    
    The hyperparameters are encoded with fake values.
    The code is still under development.

--- src/base.py
+++ src/base.py
@@ -1,5 +1,11 @@
 """
-A basic implementation of DQN for cartpole
+A basic implementation of DQN for cartpole.
+
+Deep Q Learning with NN with
+- target network
+- constant espilon-greedy exploration
+- replay buffer
+
 """
 
 import gymnasium as gym
@@ -7,35 +13,151 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import random
-from collections import deque
 import numpy as np
 
-CHECKPOINT = "weights/dqn.pth"
+from commons import QNetwork, ReplayBuffer, Transition
+from pprint import pprint
 
-#FIXME import from commons
-class QNetwork(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(QNetwork, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, action_dim)
+#FIXME use the literature or references for the hyperparameters values
+CONFIG = {
+    "checkpoint": "weights/baseline-dqn.pth",
+    "replay_capacity": int(1e4),
+    "batch_size": 32,
+    "learning_rate": 3e-4,
+    "epsilon": 0.0,
+    "episodes": 2,
+    "max_steps": 500,
+    "max_steps": 500,
+    "target_upd_steps": 4
+}
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)
+def optimize_qnet(optimizer,
+                  q_net:QNetwork,
+                  t_net:QNetwork,
+                  buffer:ReplayBuffer) -> None:
+    pass
+# optimize_qnet
 
-# setup the environment
-env = gym.make('CartPole-v1')
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.n
+def behavior_policy(q_net: QNetwork, state: torch.tensor, action_dim: int) -> int:
+    """
+    Select an action based on epsilon-greedy exploration method.
+    return action
+    """
+    global CONFIG
+    assert state.dim() == 1
 
-# Init policy and target network with the same weights
-q_net = QNetwork(state_dim, action_dim)
-target_net = QNetwork(state_dim, action_dim)
-target_net.load_state_dict(q_net.state_dict())
+    action: int = 0
+    p: float = random.random()
 
-#FIXME training loop
+    if p < CONFIG["epsilon"]:
+        # a ~ A
+        action = random.choice(range(action_dim))
+    else:
+        # a = argmax Q(s,a)
+        action = q_net(state.unsqueeze(0)).max(1).indices.item()
 
-torch.save(q_net.state_dict(), CHECKPOINT)
-print(f"Model saved as {CHECKPOINT}")
+    assert type(action) is int
+    return action
+# behavior_policy
+
+def main(device: str):
+    global CONFIG
+    print(f"Running on device {device}")
+    # setup the environment
+    env = gym.make('CartPole-v1')
+    #env = gym.make('CartPole-v1', render_mode="human")
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+
+    assert state_dim == 4
+    assert action_dim == 2
+
+    # Init the experience replay buffer
+    buffer = ReplayBuffer(capacity=CONFIG["replay_capacity"])
+    # Init policy and target network with the same weights
+    q_net = QNetwork(state_dim, action_dim)
+    t_net = QNetwork(state_dim, action_dim)
+    t_net.load_state_dict(q_net.state_dict())
+
+    # only the state-action value function is upgraded by SDG
+    optimizer = optim.AdamW(q_net.parameters(),
+                            lr=CONFIG["learning_rate"],
+                            amsgrad=True)
+
+    for episode in range(CONFIG["episodes"]):
+        print(f"Start episode {episode}...")
+
+        state, info = env.reset()
+        state = torch.tensor(state, dtype=torch.float32, device=device)
+
+        finished = False
+        steps = 0      # episode steps
+        steps_tot = 0  # global number of steps, for t_net update
+        # run an episode until the agent fails or it reaches max_steps
+        while not finished:
+            action: int = behavior_policy(q_net, state, action_dim)
+            obs, reward, terminated, *_ = env.step(action)
+            # truncated is not taken into account in this setup
+            finished = terminated
+
+            # assign next state
+            # None is a flag for avoiding calculating the max Q in terminal states
+            next_state = None
+            if not terminated:
+                next_state = torch.tensor(obs, dtype=torch.float32, device=device)
+
+            trans: Transition = Transition(state=state,
+                                           action=torch.tensor([action],
+                                                               dtype=torch.long,
+                                                               device=device),
+                                           next_state=next_state,
+                                           reward=torch.tensor([reward],
+                                                               dtype=torch.long,
+                                                               device=device))
+            # save in the experience replay buffer
+            buffer.push(trans)
+
+            optimize_qnet(optimizer, q_net, t_net, buffer)
+
+            # clone Q into the target network at fixed steps interval,
+            # regardless the relative position in the episode.
+            steps_tot += 1
+            if steps_tot % CONFIG["target_upd_steps"] == 0:
+                t_net.load_state_dict(q_net.state_dict())
+                print("Target Net updated")
+
+
+            # while loop conditions
+            state = next_state
+            steps += 1
+            if steps >= CONFIG["max_steps"]:
+                finished = True
+        # while finished
+        print(f"Terminated episode {episode} with {steps=}")
+    # for episodes
+
+    #FIXME training loop
+
+#FIXME RESTORE
+#    torch.save(q_net.state_dict(), CHECKPOINT)
+#    print(f"Model saved as {CHECKPOINT}")
+
+if __name__ == '__main__':
+    print("Hyperparameters")
+    pprint(CONFIG)
+
+    # set random seeds for reproducibility
+    # It affects only the agent code, the environment is still unpredictable
+    seed: int = 42
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    # if GPU is to be used
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else
+        "mps" if torch.backends.mps.is_available() else
+        "cpu"
+    )
+
+    main(device)
---
+++ src/commons.py
@@ -0,0 +1,58 @@
+"""
+Common parts to use in the different training setups
+"""
+import torch
+import torch.nn as nn
+from collections import namedtuple, deque
+import random
+
+# Inspired by
+# https://docs.pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+# Each component is a tensor
+Transition = namedtuple('Transition',
+                        ['state', 'action', 'next_state', 'reward'])
+
+# This is the 'standard' neural network
+class QNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+
+class ReplayBuffer():
+    """
+    Exeperience Replay Buffer of transitions.
+    The transitions are stored as tuple of
+    (state, action, next_state, reward).
+    A sample method is provided to uniformily choose a batch of transitions.
+    """
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.buffer = deque([], maxlen=self.capacity)
+
+    def push(self, transition: Transition) -> None:
+        """
+        Add the transition in the buffer.
+        If the buffer is full, the oldest transition will be discarded
+        """
+        self.buffer.append(transition)
+
+    def sample(self, batch_size: int) -> list[Transition]:
+        """
+        Uniform sample of batch_size transitions
+        """
+        # Return a k length list of unique elements chosen
+        # from the population sequence. (without replacement)
+        assert batch_size <= len(self.buffer)
+
+        return random.sample(self.buffer, batch_size)
+
+    def __len__(self):
+        return len(self.buffer)
